b8b7866238018db0bc8706ab67f38f426a6f37cd
vasu69wow <83006153+vasu69wow@users.noreply.github.com>
2021-04-22 15:28:23 +0530

    Add files via upload

---
+++ js/cursor.js
@@ -0,0 +1,78 @@
+
+/*==================== SHOW MENU ====================*/
+
+
+
+
+/*==================== SHOW MENU ====================*/
+
+
+$( "#target" ).click(function(){
+  $('body').toggleClass('dark-theme');
+  $('#theme-button').toggleClass('fa-sun');
+ });
+
+// var cursor = document.querySelector(".cursor");
+//     var cursor2 = document.querySelector(".cursor2");
+//     document.addEventListener("mousemove",function(e){
+//       cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+//     });
+
+var currentMousePos = { x: -1, y: -1 };
+
+$(document).ready(function() {
+
+    $("body").hover(function(){
+        $(".cursor-inner, .cursor-outer").addClass("show");
+    }, function () {
+        $(".cursor-inner, .cursor-outer").removeClass("show");
+    });
+
+    $(window).mousemove(function(event) {
+        $(".cursor-inner, .cursor-outer").addClass("show");
+        var currentX = currentMousePos.x = event.pageX;
+        var currentY = currentMousePos.x = event.pageY - $(window).scrollTop();
+        $(".cursor-inner").css({"transform":"translate3d(" + currentX + "px, " + currentY + "px, 0)"});
+        setTimeout(function(){
+            $(".cursor-outer").css({"transform":"translate3d(" + currentX + "px, " + currentY + "px, 0)"});
+        }, 100);
+    });
+    
+    $(".activeMouse").hover(function(){
+        $(".cursor-outer").addClass("active");
+    }, function () {
+        $(".cursor-outer").removeClass("active");
+    });
+
+    $(".activemousebig").hover(function(){
+        $(".cursor-outer").addClass("activebig");
+    }, function () {
+        $(".cursor-outer").removeClass("activebig");
+    });
+    
+}); 
+
+
+     function y(t, e) {
+            (null == e || e > t.length) && (e = t.length);
+            for (var i = 0, n = new Array(e); i < e; i++) n[i] = t[i];
+            return n;
+        }
+        var d = {
+               el: document,
+                name: "scroll",
+                offset: [0, 0],
+                repeat: !1,
+                smooth: !1,
+                direction: "vertical",
+                gestureDirection: "vertical",
+                reloadOnContextChange: !1,
+                lerp: 0.1,
+                class: "is-inview",
+            }
+
+
+
+
+
+
